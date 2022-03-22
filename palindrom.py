a5c3da2cc6b5dfd335d3833815c85f0411bc3ed9
Piotr Dzieszkowski <piotr.dzieszkowski@gmail.com>
2022-03-22 09:18:50 +0000

    zadanie.polindrom

---
+++ palindrom.py
@@ -0,0 +1,10 @@
+def palindrome(lista):
+    wynik = list()
+    for text in lista:
+        if text[::-1] == text:
+            wynik.append(True)
+        else:
+            wynik.append(False)
+    return wynik
+ 
+print(palindrome(["mama", "kajak", "książka","potop", "lalka"]))
